2cb21dfbf3e2ed61b0cc4b649dcde41d8c4b25c2
student <smodarressi7@gmail.com>
2021-01-30 16:26:04 +0000

    first commit

---
+++ src/main/java/com/example2/simplilearn/demo/DemoSimplilearnApplication.java
@@ -0,0 +1,13 @@
+package com.example2.simplilearn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSimplilearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSimplilearnApplication.class, args);
+	}
+
+}
